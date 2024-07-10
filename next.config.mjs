39eef7bc06393504d8b2dbb50393e877f7ac88ee
gahara086 <128924106+gahara086@users.noreply.github.com>
2024-07-10 10:13:52 +0900

    Update next.config.mjs
    fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    // 他の環境変数もここに追加します
+  },
 };
 
 export default nextConfig;
