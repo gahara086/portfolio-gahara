e2114ef869db26fdebf94d0bf1551688fdfd40e5
gahara <moririn086@gmail.com>
2024-07-10 23:19:25 +0900

    fix auth error

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // 他の環境変数もここに追加します
   },
 };
